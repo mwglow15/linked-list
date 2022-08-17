5db4d9cec5e04c04a6dc972e3cdea8e402014704
mattwg <mattwglowacki@gmail.com>
2022-08-16 21:46:36 -0400

    Create initial methods

--- linked-list.rb
+++ linked-list.rb
@@ -2,8 +2,44 @@ def class LinkedList
   def initialize
     @head = nil
   end
+
+  def append(value)
+    @head = Node.new(value) if @head.nil?
+
+    tail.next_node = Node.new(value)
+  end
+
+  def prepend(value)
+
+  end
+
+  def size
+    size = 0
+    return size if @head.nil?
+    node = @head
+    while !node.next_node.nil?
+      size += 1
+      node = node.next_node
+    end
+
+    return size
+  end
+
+  def head
+    @head
+  end
+
+  def tail(node)
+    tail(node.next_node) unless node.next_node.nil?
+
+    node.next_node
+  end
+
 end
 
 def class Node
-  def initialize
+  def initialize(value = nil, next_node = nil)
+    @value = value
+    @next_node = next_node
+  end
 end
