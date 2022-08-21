3d0b0cc6d151bc3a74d8375884ccbc8078ab577d
mattwg <mattwglowacki@gmail.com>
2022-08-20 22:23:30 -0400

    Fix append, tail, and size methods.  Add pop, contains and at

--- linked-list.rb
+++ linked-list.rb
@@ -4,12 +4,17 @@ class LinkedList
   end
 
   def append(value)
-    @head = Node.new(value) if @head.nil?
-    tail = self.tail
-    tail.next_node = Node.new(value)
+    if @head.nil?
+      @head = Node.new(value)
+    else
+      node = @head
+      node = node.next_node until node.next_node.nil?
+      node.next_node = Node.new(value)
+    end
   end
 
   def prepend(value)
+    @head = Node.new(value) if @head.nil?
     node = Node.new(value)
     node.next_node = self.head
     @head = node
@@ -18,35 +23,76 @@ class LinkedList
   def size
     size = 0
     return size if @head.nil?
+
     node = @head
-    while !node.next_node.nil?
+    size += 1
+    until node.next_node.nil?
       size += 1
       node = node.next_node
     end
 
-    return size
+    size
   end
 
   def head
     @head
   end
 
-  def tail(node=@head)
-    return "head is empty"
-    tail(node.next_node) unless node.next_node.nil?
+  def tail(node = @head)
+    return "head is empty" if @head.nil?
+    
+    node = @head
+    node = node.next_node until node.next_node.nil?
 
-    node.next_node
+    node
   end
 
+  def at(index)
+    node = @head
+
+    index -= 1
+
+    index.times do
+      node = node.next_node
+    end
+    node
+  end
+
+  def pop
+    return if @head.nil?
+    node = @head
+    last_node = node
+    until node.next_node.nil?
+      last_node = node
+      node = node.next_node
+    end
+    last_node.next_node = nil
+  end
+
+  def contains(value)
+    return false if @head.nil?
+
+    node = @head
+
+    until node.nil?
+      return true if 
+    end
+  end
 end
 
 class Node
+  attr_accessor :value, :next_node
   def initialize(value = nil, next_node = nil)
     @value = value
     @next_node = next_node
   end
 end
 
-a = LinkedList.new
-a.append(1)
-p a
+b = LinkedList.new
+b.append(1)
+b.append(2)
+b.append(3)
+b.append(4)
+p b
+b.pop
+p b
